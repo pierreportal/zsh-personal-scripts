0300b1e3577d39c64c11387cb21fd13a6be7edaf
Pierre Portal <pierreportal.mac@gmail.com>
2022-06-27 13:57:54 +0200

    update configs

--- config.sh
+++ config.sh
@@ -2,6 +2,13 @@
 
 echo zsh user configs loaded.
 
+alias dev='cd ~/dev'
+alias desktop='cd ~/Desktop'
+alias _tools='cd ~/dev/_tools'
+
+alias gs='git status'
+alias gpf='git push --force-with-lease'
+alias gp1='git push --set-upstream origin master'
 # NodeJS helpers
 #
 function node-at-port {
@@ -20,38 +27,10 @@ function serve-80 {
 	python2 -m SimpleHTTPServer 80
 }
 
-function share-localhost {
+function localhost {
 	readonly port=${1:?"You must give a port"}
-	if [[ "$(which qrencode)" = "qrencode not found" ]]; then
-		echo "You need to install a dependency (qrencode) for hostshare to work. Please run 'brew install qrencode'\nDo you want to install it now? (Y/n)"
-		read
-		case $REPLY in
-			[Yy] ) brew install qrencode;;
-			[Nn] ) echo "Quit Hostshare.";;
-			* ) echo "Invalid input. please type 'y' for yes or 'n' for no";;
-		esac
-	else
-		echo "true"
-	fi
-
-	local ip=$(ifconfig | grep netmask | grep broadcast | awk '{print $2}')
-	clear
-	echo "\n\n\n
-@@@  @@@   @@@@@@    @@@@@@   @@@@@@@   @@@@@@   @@@  @@@   @@@@@@   @@@@@@@   @@@@@@@@
-@@@  @@@  @@@@@@@@  @@@@@@@   @@@@@@@  @@@@@@@   @@@  @@@  @@@@@@@@  @@@@@@@@  @@@@@@@@
-@@!  @@@  @@!  @@@  !@@         @@!    !@@       @@!  @@@  @@!  @@@  @@!  @@@  @@!     
-!@!  @!@  !@!  @!@  !@!         !@!    !@!       !@!  @!@  !@!  @!@  !@!  @!@  !@!     
-@!@!@!@!  @!@  !@!  !!@@!!      @!!    !!@@!!    @!@!@!@!  @!@!@!@!  @!@!!@!   @!!!:!  
-!!!@!!!!  !@!  !!!   !!@!!!     !!!     !!@!!!   !!!@!!!!  !!!@!!!!  !!@!@!    !!!!!:  
-!!:  !!!  !!:  !!!       !:!    !!:         !:!  !!:  !!!  !!:  !!!  !!: :!!   !!:     
-:!:  !:!  :!:  !:!      !:!     :!:        !:!   :!:  !:!  :!:  !:!  :!:  !:!  :!:     
-::   :::  ::::: ::  :::: ::      ::    :::: ::   ::   :::  ::   :::  ::   :::   :: ::::
- :   : :   : :  :   :: : :       :     :: : :     :   : :   :   : :   :   : :  : :: :: 
- \n\n
------------------------ Serving @ http://$ip:$port 👾 -----------------------
- \n\n"
- 	qrencode -s 1 -m 1 -t ANSI -o - "http://${ip}:${port}"
-	echo "\n\nThanks for using Hostshare!\n\n"
-
+	open http://localhost:$port
 }
+
+alias lh='localhost'
 
