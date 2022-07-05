29cb2b0bc555f8537c12797ec952e9a56bcd1ec0
Pierre Portal <pierreportal.mac@gmail.com>
2022-07-05 11:39:11 +0200

    v2

--- config.sh
+++ config.sh
@@ -5,10 +5,15 @@ echo zsh user configs loaded.
 alias dev='cd ~/dev'
 alias desktop='cd ~/Desktop'
 alias _tools='cd ~/dev/_tools'
-
+alias portfolio='cd ~/dev/PIERREPORTAL/portfolio/client'
 alias gs='git status'
 alias gpf='git push --force-with-lease'
 alias gp1='git push --set-upstream origin master'
+# alias ide='tmux split-window -v & tmux split-window -h'
+source ~/.zsh_user_config/tmux/ide
+
+
+export GITHUB_GRAPH_API=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
 # NodeJS helpers
 #
 function node-at-port {
@@ -32,5 +37,14 @@ function localhost {
 	open http://localhost:$port
 }
 
+function tunnelPort {
+	readonly port=${1:?"You must give a port"}
+	local href=$(lt --port $port)&
+	echo $href
+	qrencode -s 1 -m 1 -t ANSI -o - "${href}"
+}
+
 alias lh='localhost'
+
+source ~/dev/_tools/hostshare/hostshare.sh
 
